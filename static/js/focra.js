664a4a7dd9e5d66271220455ee7b36f8ac036e24
Jayden <mingsheng36@gmail.com>
2014-12-12 00:15:15 +0800

    Modified User Interface

--- static/js/focra.js
+++ static/js/focra.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
-	$("#start").click( function(event) {
+	// call to start crawler
+	$('#startBn').click(function(event) {
 		$('#display').html('Initializing..');
-		$.get('/start/', function(data){
-			$('#display').html(data);
-		});
 	});
-
-	$("#stop").click( function(event) {
+	
+	// call to stop crawler
+	$('#stopBn').click( function(event) {
 		$('#display').html('Terminating..');
 		$.get('/stop/', function(data){
 			$('#display').html(data);
 		});
 	});
+	
+	// show and hide div
+	$('a[href="#createCrawler"]').click(function(){
+		$("#createDiv").fadeIn("fast", "linear");
+	});
+	
 });
 
+//$.ajax({
+//    url : /start/,
+//    type: 'POST',
+//    data : {csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
+//    		crawlerName : $('#crawlerName').val(), 
+//    		crawlerSeeds : $('#crawlerSeeds').val()},
+//    success: function( data ){
+//    	$('#display').html(data);
+//    	$("#createDiv").hide();
+//    }
+//});
